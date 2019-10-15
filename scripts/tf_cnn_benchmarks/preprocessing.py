fca98386c777547b0e113363fbf63b25ed3e9715
Claudio Fahey <claudio.fahey@dell.com>
2019-10-15 23:38:52 +0000

    Removed unnecessary file globbing.

--- scripts/tf_cnn_benchmarks/preprocessing.py
+++ scripts/tf_cnn_benchmarks/preprocessing.py
@@ -653,11 +653,7 @@ class BaseImagePreprocessor(InputPreprocessor):
     """Creates a dataset for the benchmark."""
     assert self.supports_datasets()
     glob_pattern = dataset.tf_record_pattern(subset)
-    file_names = gfile.Glob(glob_pattern)
-    if not file_names:
-      raise ValueError('Found no files in --data_dir matching: {}'
-                       .format(glob_pattern))
-    ds = tf.data.TFRecordDataset.list_files(file_names, shuffle=train)
+    ds = tf.data.TFRecordDataset.list_files(glob_pattern, shuffle=train)
     ds = ds.apply(
         tf.data.experimental.parallel_interleave(
             tf.data.TFRecordDataset,
